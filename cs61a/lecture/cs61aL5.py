0f10776456d2386506516cd1a6baf58331f28d88
Wenjie Jiang <wenjie.jiang@quokka.se>
2022-02-21 16:08:50 +0100

    L5

---
+++ cs61a/lecture/cs61aL5.py
@@ -0,0 +1,52 @@
+from re import X
+
+
+def apply_twice(f,x):
+    return f(f(x))
+
+def square(x):
+    return  x * x
+
+def repeat(f,x):
+    """
+    >>> repeat(g,5)
+    2
+    """
+    while f(x) != x:
+        x = f(x)
+    return x
+
+def g(y):
+    return (y+5) // 3
+
+def make_adder(n):
+    """
+    >>> make_adder(3)(8)
+    11
+    """
+    def adder(k):
+
+        return k + n
+    return adder
+
+
+def triple(x):
+    return 3 * x
+
+def compose1(f,g):
+    """
+    >>> compose1(square, make_adder(2))(3)
+    25
+    """
+    def h(x):
+        return f(g(x))
+    return h
+
+def curry2(f):
+    def g(x):
+        def h(y):
+            return f(x,y)
+        return h
+    return g
+
+
