720916c9a11506a841ba960aa9db69fe418a7400
benigk <77833646+benigk@users.noreply.github.com>
2022-10-02 20:21:15 +0200

    A4

---
+++ LT2001/Assignment/A4/A4.py
@@ -0,0 +1,13 @@
+name = {'n1': 'Gerlof von Bayes', 'n2': 'Arrietty Borrower', 'n3': 'Amélie Poulain', 'n4': 'Pi Thon-Rappel'}
+print(name)
+
+kindergarten = {
+                'Gerlof':{'birthday': '2016-12-31', 'soc.sec.no':'4567', 'topic':'syntax, historical linguistics, logic programming and spelling'},
+                'Arrietty':{'birthday':'2017-07-17', 'soc.sec.no':'2345', 'topic': 'loan words'}, 
+                'Amélie':{'birthday': '2015-03-03', 'soc.sec.no':'0123', 'topic': 'NLP for Parisian French, sand castle engineering'}, 
+                'Pi':{'birthday':'2015-02-29', 'soc.sec.no':'8901', 'topic': 'programming language design'},
+                }
+
+
+##print(kindergarten['Gerlof']['birthday'])
+kindergarten.items()
---
+++ LT2001/Assignment/A4/A41.py
@@ -0,0 +1,2 @@
+name = {'n1': 'Gerlof von Bayes', 'n2': 'Arrietty Borrower', 'n3': 'Amélie Poulain', 'n4': 'Pi Thon-Rappel'}
+print(name)
